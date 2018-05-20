6686e8853866614a186316e147accc61e8cb81a2
leordev <leordev@gmail.com>
2018-05-20 09:26:58 -0400

    feeding the pet working

--- pet/pet.cpp
+++ pet/pet.cpp
@@ -20,6 +20,25 @@ typedef uint64_t uuid;
 
 // There's five pet types
 const uint8_t PET_TYPES = 5;
+const uint32_t DAY = 86400;
+const uint32_t TWENTY_HOURS = 72000;
+const uint8_t  MAX_HEALTH = 100;
+const uint32_t HUNGER_TO_ZERO = 600; // DAY
+const uint8_t  MAX_HUNGER_POINTS = 100;
+const uint8_t  HUNGER_HP_MODIFIER = 1;
+const uint8_t  HUNGER_FEED_POINTS = 33;
+const uint32_t HAPPINESS_TO_ZERO = 2 * DAY;
+const uint8_t  MAX_HAPPINESS_POINTS = 100;
+const uint8_t  HAPPINESS_HP_MODIFIER = 2;
+const uint8_t  HAPPINESS_PLAY_POINTS = 33;
+const uint32_t AWAKE_TO_ZERO = TWENTY_HOURS;
+const uint8_t  MAX_AWAKE_POINTS = 100;
+const uint8_t  AWAKE_BED_POINTS = 33;
+const uint8_t  AWAKE_HP_MODIFIER = 2;
+const uint32_t SHOWER_TO_ZERO = DAY;
+const uint8_t  MAX_CLEAN_POINTS = 100;
+const uint8_t  CLEAN_HP_MODIFIER = 3;
+const uint8_t  CLEAN_SHOWER_POINTS = 33;
 
 uint128_t combine_ids(uint64_t const& x, uint64_t const& y) {
     uint128_t times = 1;
@@ -53,16 +72,6 @@ public:
 
         st_pet_config pc = _get_pet_config();
 
-        // TODO: Utilize receipt eosio::transfer to manage account balances
-//        // Pay for Pet Creation Fee
-//        if (pc.creation_fee > asset{}) {
-//            action(
-//                permission_level{ owner, N(active) },
-//                N(eosio.token), N(transfer),
-//                std::make_tuple(owner, _self, pc.creation_fee, std::string("PET - Creation Fee"))
-//            ).send();
-//        }
-
         print("creating pet");
 
         // creates the pet
@@ -81,11 +90,11 @@ public:
 
             r = pet;
 
-            // testing deferred
-            transaction out{};
-            out.actions.emplace_back(permission_level{_self, N(active)}, N(pet), N(updatepet), std::make_tuple(pet.id, 1));
-            out.delay_sec = 1;
-            out.send(pet.id, _self);
+//            // testing deferred
+//            transaction out{};
+//            out.actions.emplace_back(permission_level{_self, N(active)}, N(pet), N(updatepet), std::make_tuple(pet.id, 1));
+//            out.delay_sec = 1;
+//            out.send(pet.id, _self);
         });
     }
 
@@ -93,21 +102,53 @@ public:
         require_auth(_self);
         print(pet_id, "|", iteration, ": updating pet ");
 
-//        int block_num = tapos_block_num();
-//        print("\ntapos_block_num = ", block_num);
+        auto itr_pet = pets.find(pet_id);
+        eosio_assert(itr_pet != pets.end(), "E404|>Invalid pet");
+        st_pets pet = *itr_pet;
+
+        _update(pet);
+
+        pets.modify(itr_pet, 0, [&](auto &r) {
+            r.health = pet.health;
+            r.hunger = pet.hunger;
+            r.awake = pet.awake;
+            r.happiness = pet.happiness;
+            r.clean = pet.clean;
+        });
 
         // testing deferred recursive
-        transaction out{};
-        out.actions.emplace_back(
-                permission_level{_self, N(active)},
-                N(pet), N(updatepet),
-                std::make_tuple(pet_id, iteration+1));
-        out.delay_sec = 1;
-        out.send(combine_ids(pet_id, uint64_t{iteration}), _self);
+        // does not look a good idea... better to have
+        // a cronjob doing that for all pets maybe? :P
+//        transaction out{};
+//        out.actions.emplace_back(
+//                permission_level{_self, N(active)},
+//                N(pet), N(updatepet),
+//                std::make_tuple(pet_id, iteration+1));
+//        out.delay_sec = 60;
+//        out.send(combine_ids(pet_id, uint64_t{iteration}), _self);
     }
 
     void feedpet(uuid pet_id, uint32_t iteration) {
-        print("feed lazy developer");
+
+        auto itr_pet = pets.find(pet_id);
+        eosio_assert(itr_pet != pets.end(), "E404|>Invalid pet");
+        st_pets pet = *itr_pet;
+
+        _update(pet);
+
+        pets.modify(itr_pet, 0, [&](auto &r) {
+            r.health = pet.health;
+            r.hunger = pet.hunger;
+            r.awake = pet.awake;
+            r.happiness = pet.happiness;
+            r.clean = pet.clean;
+
+            if (r.health > 0) {
+                r.hunger = (pet.hunger + HUNGER_FEED_POINTS) > MAX_HUNGER_POINTS ?
+                        MAX_HUNGER_POINTS : (pet.hunger + HUNGER_FEED_POINTS);
+                r.last_fed_at = now();
+            }
+        });
     }
 
     void bedpet(uuid pet_id) {
@@ -136,15 +177,16 @@ private:
         name owner;
         string name;
         uint8_t type;
+        uint32_t created_at;
+        uint32_t death_at = 0;
         uint8_t health = 100;
         uint8_t hunger = 70;
-        uint8_t awake = 100;
-        uint8_t happiness = 70;
-        uint8_t clean = 100;
-        uint32_t created_at;
         uint32_t last_fed_at;
-        uint32_t last_play_at;
+        uint8_t awake = 100;
         uint32_t last_bed_at;
+        uint8_t happiness = 70;
+        uint32_t last_play_at;
+        uint8_t clean = 100;
         uint32_t last_shower_at;
 
         uint64_t primary_key() const { return id; }
@@ -193,6 +235,38 @@ private:
 
     uint64_t _hash_str(const string &str) {
         return hash<string>{}(str);
+    }
+
+    void _update(st_pets &pet) {
+
+        eosio_assert(pet.health > 0 || pet.death_at > 0, "E099|>Pet is dead");
+
+        uint32_t current_time = now();
+
+        uint8_t effect_hp_hunger = _calc_hunger_hp(pet, current_time);
+
+        pet.health = MAX_HEALTH - effect_hp_hunger;
+
+        if (pet.health <= 0) {
+            pet.death_at = now();
+        }
+    }
+
+    uint8_t _calc_hunger_hp(st_pets &pet, const uint32_t &current_time) {
+        // how long it's hungry?
+        uint32_t hungry_seconds = current_time - pet.last_fed_at;
+        uint8_t hungry_points = (uint8_t) (hungry_seconds * MAX_HUNGER_POINTS / HUNGER_TO_ZERO);
+
+        // calculates the effective hunger on hp, if pet hunger is 0
+        uint8_t effect_hp_hunger = 0;
+        if (hungry_points < pet.hunger) {
+            pet.hunger -= hungry_points;
+        } else {
+            effect_hp_hunger = (uint8_t) (hungry_points - (pet.hunger / HUNGER_HP_MODIFIER));
+            pet.hunger = 0;
+        }
+
+        return effect_hp_hunger;
     }
 
 

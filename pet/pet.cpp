f11be2da2830a82827359fb0619299b780e21335
leordev <leordev@gmail.com>
2018-05-19 21:43:05 -0400

    pet tamagotchi alike

--- pet/pet.cpp
+++ pet/pet.cpp
@@ -17,17 +17,8 @@ using std::hash;
 
 typedef uint64_t uuid;
 
-
-
-
-/* ****************************************** */
-/* ------------ Helper Functions ------------ */
-/* ****************************************** */
-
-
-uuid gen_uuid(const uint64_t &id) {
-    return hash<uint64_t>{}(id);
-}
+// There's five pet types
+const uint8_t PET_TYPES = 5;
 
 
 
@@ -48,33 +39,58 @@ public:
     /* ------------ Contract Actions ------------ */
     /* ****************************************** */
 
-    void createpet(account_name owner,
-                   string name) {
+    void createpet(name owner,
+                   string pet_name) {
 
         require_auth(owner);
 
         st_pet_config pc = _get_pet_config();
 
-        // Pay for Pet Creation Fee
-        if (pc.creation_fee > asset{}) {
-            action(
-                permission_level{ owner, N(active) },
-                N(eosio.token), N(transfer),
-                std::make_tuple(owner, _self, pc.creation_fee, std::string("PET - Creation Fee"))
-            ).send();
-        }
+        // TODO: Utilize receipt eosio::transfer to manage account balances
+//        // Pay for Pet Creation Fee
+//        if (pc.creation_fee > asset{}) {
+//            action(
+//                permission_level{ owner, N(active) },
+//                N(eosio.token), N(transfer),
+//                std::make_tuple(owner, _self, pc.creation_fee, std::string("PET - Creation Fee"))
+//            ).send();
+//        }
+
+        print("creating pet");
 
         // creates the pet
         pets.emplace(_self, [&](auto &r) {
-            r.id = gen_uuid(_next_id());
-            r.name = name;
-            r.owner = owner;
-            r.health = pc.initial_health;
-            r.hunger = pc.initial_hunger;
-            r.created_at = now();
-            r.last_fed_at = r.created_at;
+            st_pets pet{};
+            pet.id = _next_id();
+            pet.name = pet_name;
+            pet.owner = owner;
+            pet.created_at = now();
+            pet.last_fed_at = pet.created_at;
+            pet.last_play_at = pet.created_at;
+            pet.last_bed_at = pet.created_at;
+            pet.last_shower_at = pet.created_at;
+
+            pet.type = (_hash_str(pet_name) + pet.created_at + pet.id + owner) % PET_TYPES;
+
+            r = pet;
         });
 
+    }
+
+    void feedpet() {
+        eosio_assert(false, "lazy developer");
+    }
+
+    void bedpet() {
+        eosio_assert(false, "lazy developer");
+    }
+
+    void playpet() {
+        eosio_assert(false, "lazy developer");
+    }
+
+    void washpet() {
+        eosio_assert(false, "lazy developer");
     }
 
 
@@ -88,12 +104,19 @@ private:
     // @abi table pets i64
     struct st_pets {
         uuid id;
-        account_name owner;
+        name owner;
         string name;
-        uint8_t health;
-        uint8_t hunger;
+        uint8_t type;
+        uint8_t health = 100;
+        uint8_t hunger = 70;
+        uint8_t awake = 100;
+        uint8_t happiness = 70;
+        uint8_t clean = 100;
         uint32_t created_at;
         uint32_t last_fed_at;
+        uint32_t last_play_at;
+        uint32_t last_bed_at;
+        uint32_t last_shower_at;
 
         uint64_t primary_key() const { return id; }
     };
@@ -110,8 +133,6 @@ private:
     struct st_pet_config {
         uuid last_id = 1000000000;
         asset creation_fee = asset{10000,S(4,EOS)};
-        uint8_t initial_health = 100;
-        uint8_t initial_hunger = 100;
     };
 
     typedef singleton<N(pet_config), st_pet_config> pet_config_singleton;
@@ -139,6 +160,10 @@ private:
         pc.last_id++;
         pet_config.set(pc, _self);
         return pc.last_id;
+    }
+
+    uint64_t _hash_str(const string &str) {
+        return hash<string>{}(str);
     }
 
 
